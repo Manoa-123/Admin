63d356fb7003589046ff089bc51996e6c1a168da
Manoa <147591719+Manoa-123@users.noreply.github.com>
2024-06-06 14:13:52 +0400

    Update modifier_acceuil.php

--- modifier_acceuil.php
+++ modifier_acceuil.php
@@ -8,7 +8,7 @@ if (isset($_GET['id_acceuil'])) {
     exit(); // Arrêtez le script pour éviter l'affichage du formulaire vide
 }
 // Connection à la base
-$conn = mysqli_connect("localhost", "root", "", "supercar");
+ include('bdd/conexion_bdd.php');
 
 // Récupérez les données actuelles depuis la base de données
 $sql = "SELECT * FROM acceuil WHERE id_acceuil = $id_acceuil";
